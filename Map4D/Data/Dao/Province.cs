8811058f2908b92607b60469b97f9631c54ce016
vovanhung <45864070+tryit4you@users.noreply.github.com>
2019-01-16 11:03:49 +0700

    create data access layer

---
+++ Map4D/Data/Dao/Province.cs
@@ -0,0 +1,18 @@
+﻿using CommonLogger.Libraries;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Map4D.Data.Dao
+{
+    public class Province:AdoHelper
+    {
+        AdoHelper helper = null;
+        public Province()
+        {
+            string connection = AdoHelper.ConnectionString;
+            helper = new AdoHelper(connection);
+        }
+    }
+}
