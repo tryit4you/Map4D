a689bff9d54cca2878ee1ee9391ed718d25649af
vovanhung <45864070+tryit4you@users.noreply.github.com>
2019-01-16 15:08:49 +0700

    fisnish code draw polygon

--- Map4D/Controllers/DrawPolygonController.cs
+++ Map4D/Controllers/DrawPolygonController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Map4D.Data.BO;
+using Map4D.Models;
+using Map4D.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +11,60 @@ namespace Map4D.Controllers
 {
     public class DrawPolygonController : Controller
     {
+        private DrawPolygonBo drawPolygonBo = new DrawPolygonBo();
         // GET: DrawPolygon
-        public ActionResult Index()
+        public ActionResult Index(string cityId,string districtId)
         {
-            return View();
+            string currentCityId=string.Empty;
+            string currentDistrictId=string.Empty;
+
+            IEnumerable<CountriesViewModel> listDistricts=null;
+            IEnumerable<CountriesViewModel> listWards=null;
+            if (cityId!=null)
+            {
+                currentCityId = cityId;
+                listDistricts = drawPolygonBo.GetAllWardByDistrict(int.Parse(cityId));
+            }
+            if (districtId != null)
+            {
+                currentDistrictId = districtId;
+                listWards = drawPolygonBo.GetAllWardByDistrict(int.Parse(districtId));
+            }
+
+            var listCity = drawPolygonBo.GetAllCity();
+            var DrawPolygonViewModel = new DrawPolygonViewModels
+            {
+                CurrentCityId=currentCityId,
+                CurrentDistrictId=currentDistrictId,
+                Cities = listCity,
+                Districts = listDistricts,
+                Wards = listWards,
+            };
+            return View(DrawPolygonViewModel);
+        }
+        public JsonResult ListCity()
+        {
+            var listCity = drawPolygonBo.GetAllCity();
+            return Json(new
+            {
+                data = listCity
+            },JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult ListDistrict(string cityId)
+        {
+            var listDistrict = drawPolygonBo.GetAllDistrictByCity(int.Parse(cityId));
+            return Json(new
+            {
+                data = listDistrict
+            },JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult ListWard(string districtId)
+        {
+            var listWard = drawPolygonBo.GetAllWardByDistrict(int.Parse(districtId));
+            return Json(new
+            {
+                data = listWard
+            },JsonRequestBehavior.AllowGet);
         }
     }
 }
