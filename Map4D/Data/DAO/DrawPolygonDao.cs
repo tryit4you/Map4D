9195d1da6e4e1f0e43e92241d8a0eaa664374bd1
Phạm Minh Đức <42537338+pmduc97@users.noreply.github.com>
2019-01-16 13:43:01 +0700

    Create DrawPolygonDao and DrawPolygonBo

---
+++ Map4D/Data/DAO/DrawPolygonDao.cs
@@ -0,0 +1,109 @@
+﻿using CommonLogger.Libraries;
+using Map4D.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace Map4D.Data.DAO
+{
+    public class DrawPolygonDao:AdoHelper
+    {
+        private AdoHelper helper = null;
+        /// <summary>
+        /// Construct Open SqlConnection
+        /// </summary>
+        public DrawPolygonDao()
+        {
+            string connection = AdoHelper.ConnectionString;
+            helper = new AdoHelper(connection);
+        }
+        /// <summary>
+        /// Get all City in VietNam
+        /// </summary>
+        /// <returns>List<CountriesViewModel></returns>
+        public List<CountriesViewModel> GetAllCity()
+        {
+            List<CountriesViewModel> listCity = new List<CountriesViewModel>();
+            string sqlQuery = "SELECT * FROM Countries WHERE Level = 1 ORDER BY Name";
+            SqlDataReader reader = helper.ExecDataReader(sqlQuery);
+            while (reader.Read())
+            {
+                CountriesViewModel City = new CountriesViewModel
+                {
+                    Id = int.Parse(reader["Id"].ToString()),
+                    Name = reader["Name"].ToString(),
+                    Description = reader["Description"].ToString(),
+                    Level = int.Parse(reader["Level"].ToString()),
+                    Type = reader["Type"].ToString(),
+                    ParentId = int.Parse(reader["ParentId"].ToString()),
+                    IsVisible = int.Parse(reader["IsVisible"].ToString()),
+                    IsState = bool.Parse(reader["IsState"].ToString()),
+                    NameKhongDau = reader["NameKhongDau"].ToString()
+                };
+                listCity.Add(City);
+            }
+            reader.Close();
+            return listCity;
+        }
+        /// <summary>
+        /// Get all District by City
+        /// </summary>
+        /// <param name="IdCity"></param>
+        /// <returns>List<CountriesViewModel></returns>
+        public List<CountriesViewModel> GetAllDistrictByCity(int IdCity)
+        {
+            List<CountriesViewModel> listDistrict = new List<CountriesViewModel>();
+            string sqlQuery = "SELECT * FROM Countries WHERE ParentId = " + IdCity.ToString() + "  ORDER BY Name";
+            SqlDataReader reader = helper.ExecDataReader(sqlQuery);
+            while (reader.Read())
+            {
+                CountriesViewModel District = new CountriesViewModel
+                {
+                    Id = int.Parse(reader["Id"].ToString()),
+                    Name = reader["Name"].ToString(),
+                    Description = reader["Description"].ToString(),
+                    Level = int.Parse(reader["Level"].ToString()),
+                    Type = reader["Type"].ToString(),
+                    ParentId = int.Parse(reader["ParentId"].ToString()),
+                    IsVisible = int.Parse(reader["IsVisible"].ToString()),
+                    IsState = bool.Parse(reader["IsState"].ToString()),
+                    NameKhongDau = reader["NameKhongDau"].ToString()
+                };
+                listDistrict.Add(District);
+            }
+            reader.Close();
+            return listDistrict;
+        }
+        /// <summary>
+        /// Get all Ward by District
+        /// </summary>
+        /// <param name="IdDistrict"></param>
+        /// <returns>List<CountriesViewModel></returns>
+        public List<CountriesViewModel> GetAllWardByDistrict(int IdDistrict)
+        {
+            List<CountriesViewModel> listWard = new List<CountriesViewModel>();
+            string sqlQuery = "SELECT * FROM Countries WHERE ParentId = " + IdDistrict.ToString() + "  ORDER BY Name";
+            SqlDataReader reader = helper.ExecDataReader(sqlQuery);
+            while (reader.Read())
+            {
+                CountriesViewModel Ward = new CountriesViewModel
+                {
+                    Id = int.Parse(reader["Id"].ToString()),
+                    Name = reader["Name"].ToString(),
+                    Description = reader["Description"].ToString(),
+                    Level = int.Parse(reader["Level"].ToString()),
+                    Type = reader["Type"].ToString(),
+                    ParentId = int.Parse(reader["ParentId"].ToString()),
+                    IsVisible = int.Parse(reader["IsVisible"].ToString()),
+                    IsState = bool.Parse(reader["IsState"].ToString()),
+                    NameKhongDau = reader["NameKhongDau"].ToString()
+                };
+                listWard.Add(Ward);
+            }
+            reader.Close();
+            return listWard;
+        }
+    }
+}
