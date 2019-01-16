00b62ae64ae9d7aca2eba2e50775cc51ba675043
vovanhung <45864070+tryit4you@users.noreply.github.com>
2019-01-16 15:08:49 +0700

    remove comment

--- Map4D/Assets/js/drawpolygon.js
+++ Map4D/Assets/js/drawpolygon.js
@@ -1,8 +1,22 @@
 ﻿$(function () {
     register();
+    getListCity();
 });
 function register() {
     $('#metismenu').metisMenu({
         toggle: false
+    });
+}
+function getListCity() {
+    $.ajax({
+        url: '/drawpolygon/listcity',
+        type: 'post',
+        dataType: 'json',
+        success: function (res) {
+            var cityList = res.data;
+            if (cityList.length !== 0) {
+
+            }
+        }
     });
 }
