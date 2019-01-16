4c9e074e35da8500a655f7e9ad92575f3a475e67
Phạm Minh Đức <42537338+pmduc97@users.noreply.github.com>
2019-01-16 10:59:44 +0700

    Create ModelView

---
+++ Map4D/Models/ThongTinDoiTuongPhuViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Map4D.Models
+{
+    public class ThongTinDoiTuongPhuViewModel
+    {
+        public string Id { get; set; }
+        public string Code { get; set; }
+        public string Value { get; set; }
+        public string ThongTinDoiTuongChinhId { get; set; }
+        public string KieuDuLieu { get; set; }
+        public string Ten { get; set; }
+        public int Order { get; set; }
+    }
+}
